bc22107ccbb75c68f4f3e70c630f5d0859794354
D51947403 <singraul20codeblog@gmail.com>
2026-05-31 22:35:21 +0530

    Log4j2 yml and json configuration example.

---
+++ Log4j2YMLDemo/src/main/java/org/demo/Log4j2YmlConfig.java
@@ -0,0 +1,37 @@
+package main.java.org.demo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+//Program is not working as expected as of now.
+// need to work on this to resolve error
+public class Log4j2YmlConfig {
+    private static Logger logger = LogManager.getLogger(Log4j2YmlConfig.class);
+    public static void main(String[] args) {
+        System.out.println("=== Log4j2YmlConfig ===");
+        // Demonstrate logging to all appenders
+        demonstrateAllAppenders();
+    }
+
+    private static void demonstrateAllAppenders() {
+        logger.trace("This is a TRACE message - will appear in all configured appenders");
+        logger.debug("This is a DEBUG message - will appear in all configured appenders");
+        logger.info("This is an INFO message - will appear in all configured appenders");
+        logger.warn("This is a WARN message - will appear in all configured appenders");
+        logger.error("This is an ERROR message - will appear in all configured appenders");
+        logger.fatal("This is a FATAL message - will appear in all configured appenders");
+
+        // Demonstrate logging with parameters
+        String user = "John Doe";
+        int count = 42;
+        logger.info("User {} has performed {} operations", user, count);
+
+        // Demonstrate exception logging
+        try {
+            throw new RuntimeException("Sample exception for demonstration");
+        } catch (Exception e) {
+            logger.error("An exception occurred", e);
+        }
+
+    }
+
+}
---
+++ Log4j2YMLDemo/src/read.md
@@ -0,0 +1,9 @@
+## Link to download jackson related jars to support yaml file configuration
+
+https://repo1.maven.org/maven2/com/fasterxml/jackson/dataformat/jackson-dataformat-yaml/2.15.2/
+
+https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-core/2.15.4/
+
+https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.15.4/
+
+https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.15.0/
